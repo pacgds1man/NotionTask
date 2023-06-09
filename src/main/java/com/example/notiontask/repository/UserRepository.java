896357b8f03e882211a463fd97ac84e07904a97e
ZVER <pacgds1man@gmail.com>
2023-06-09 11:07:48 +0300

    fix

--- src/main/java/com/example/notiontask/repository/UserRepository.java
+++ src/main/java/com/example/notiontask/repository/UserRepository.java
@@ -2,7 +2,6 @@ package com.example.notiontask.repository;
 
 import com.example.notiontask.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.repository.CrudRepository;
 
 public interface UserRepository extends JpaRepository<User, Long> {
 }
